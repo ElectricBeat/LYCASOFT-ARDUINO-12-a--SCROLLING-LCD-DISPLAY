40ef4d8c63a9f06b55d89a4aec7593b40e10db99
ElectricBeat <96882916+ElectricBeat@users.noreply.github.com>
2022-03-08 21:05:15 +0530

    LYCASOFT
    LYCASOFT TRINNING CODE FOR ARDUINO UNO LCD DISPLAY CODE

---
+++ SCROLLING_LCD_DISPLAY.ino
@@ -0,0 +1,17 @@
+#include <LiquidCrystal.h> 
+//LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+LiquidCrystal lcd(10, 9, 8, 7, 6, 5);
+void setup() 
+{
+  lcd.begin(16, 2);
+}
+void loop() 
+{
+  lcd.setCursor(0,0);
+  lcd.print("Welcome to Sri Electronics");
+   delay(2000);
+   
+  lcd.scrollDisplayLeft();
+  delay(500);
+}
